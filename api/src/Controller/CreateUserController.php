b4f725a7c4fb9f9601941b0ef7a1a2e74dd55c82
PolCampru <pol.camprubi.prats@estudiantat.upc.edu>
2022-10-20 01:37:45 +0200

    Completed video 1 Next: do video 2

--- api/src/Controller/CreateUserController.php
+++ api/src/Controller/CreateUserController.php
@@ -8,11 +8,39 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CreateUserController extends AbstractController
 {
-    #[Route('/create/user', name: 'app_create_user')]
-    public function index(): Response
+    #[Route('/users/register', name: 'app_create_user')]
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
-        return $this->render('create_user/index.html.twig', [
-            'controller_name' => 'CreateUserController',
+        // username will be email
+        $email = $request->get("username");
+        $user = $this->userRepository->findOneBy([
+            'email' => $email,
         ]);
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+
+        if (!is_null($user)) {
+            $returnMessage = json_encode(["message"=>"User already exists"]);
+            $response->setContent($returnMessage);
+            $response->setStatusCode(401);
+            return $response;
+        }
+        // ... e.g. get the user data from a registration form
+        $user = new User();
+        $plaintextPassword = $request->get("password");
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setEmail($email);
+        $user->setPassword($hashedPassword);
+
+
+        //$response->setContent()
+        //$response->setStatusCode(201);
+        return $user;
     }
 }
--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -30,6 +32,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column]
     private ?string $password = null;
+
+    #[ORM\OneToMany(mappedBy: 'tokenOwner', targetEntity: ApiToken::class, orphanRemoval: true)]
+    private Collection $apiTokens;
+
+    public function __construct()
+    {
+        $this->apiTokens = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -99,5 +109,35 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    /**
+     * @return Collection<int, ApiToken>
+     */
+    public function getApiTokens(): Collection
+    {
+        return $this->apiTokens;
+    }
+
+    public function addApiToken(ApiToken $apiToken): self
+    {
+        if (!$this->apiTokens->contains($apiToken)) {
+            $this->apiTokens->add($apiToken);
+            $apiToken->setTokenOwner($this);
+        }
+
+        return $this;
+    }
+
+    public function removeApiToken(ApiToken $apiToken): self
+    {
+        if ($this->apiTokens->removeElement($apiToken)) {
+            // set the owning side to null (unless already changed)
+            if ($apiToken->getTokenOwner() === $this) {
+                $apiToken->setTokenOwner(null);
+            }
+        }
+
+        return $this;
     }
 }
