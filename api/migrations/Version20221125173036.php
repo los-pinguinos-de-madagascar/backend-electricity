5be3d9a92b39fd75e55d5de91edacda984ad3438
Pol Blavia <pol.blavia@estudiantat.upc.edu>
2022-11-25 18:39:06 +0100

    Migration per fer merge Locations

--- api/migrations/Version20221125173036.php
+++ api/migrations/Version20221125173036.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221124195117 extends AbstractMigration
+final class Version20221125173036 extends AbstractMigration
 {
     public function getDescription(): string
     {
