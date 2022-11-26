0771042d88fff5497500c9120c96903598ac0349
David Herrero <david.herrero@webfleet.com>
2022-11-26 15:53:52 +0100

    Migrations fix

--- api/migrations/Version20221126144729.php
+++ api/migrations/Version20221126144729.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221125173036 extends AbstractMigration
+final class Version20221126144729 extends AbstractMigration
 {
     public function getDescription(): string
     {
