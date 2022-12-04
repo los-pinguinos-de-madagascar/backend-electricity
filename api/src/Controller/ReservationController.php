6806b2b8b09d4a9fbdc6373866d0f5d6559e28d8
PolCampru <pol.camprubi.prats@estudiantat.upc.edu>
2022-12-04 14:46:35 +0100

    reservation and update reservation DONE, TODO resolve circular reference

--- api/src/Controller/ReservationController.php
+++ api/src/Controller/ReservationController.php
@@ -2,25 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Reservation;
 use App\Repository\RechargeStationRepository;
 use App\Repository\ReservationRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Monolog\DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ReservationController extends AbstractController
 {
     #[Route('/reservations', name: 'create_reservation', methods: 'POST')]
-    public function index(Request $request, RechargeStationRepository $rechargeStationRepository, ReservationRepository $reservationRepository): JsonResponse
+    public function index(Request $request, RechargeStationRepository $rechargeStationRepository, SerializerInterface $serializer,ReservationRepository $reservationRepository): JsonResponse
     {
         $requestBodyAsJSON = json_decode($request->getContent(), true);
 
         $di = $requestBodyAsJSON['dataIni'];
         $df = $requestBodyAsJSON['dataFi'];
-        $dataIni = date("d-m-Y H:i:s", strtotime($di));
-        $dataFi = date("d-m-Y H:i:s", strtotime($df));
+
+        $format = 'Y-m-d H:i:s';
+        $dataIni = DateTimeImmutable::createFromFormat($format, $di);
+        $dataFi = DateTimeImmutable::createFromFormat($format, $df);
+
         $idStation = $requestBodyAsJSON['idStation'];
         $user = $this->getUser();
 
@@ -42,23 +48,56 @@ class ReservationController extends AbstractController
         }
 
         $reservations = $reservationRepository->findBy(array('rechargeStation' => $idStation));
+        $count = 0;
+        $finalReservations = array();
+        $slots = $station->getSlots();
         foreach ($reservations as $reservation){
             if ($this->sameSpot($reservation, $dataIni, $dataFi)) {
-                $returnMessage = json_encode(["message"=>"BicingStation does not exist"],$reservation);
-                $response->setContent($returnMessage);
-                $response->setStatusCode(403);
-                return $response;
+                $count++;
+                $finalReservations[] = $reservation;
+                if($count === $slots || (($slots === null) && ($count === 1))) {
+//                    ["message" => "There are not availale slots for this date-time to reserve"]
+                    $response->headers->set('Content-Type', 'application/json');
+                    $response->setContent($serializer->serialize($finalReservations,'json'));
+                    $response->setStatusCode(403);
+                    return $response;
+                }
             }
         }
-        dd("hola");
+        $newReservation = new Reservation();
+        $newReservation->setDataIni($dataIni);
+        $newReservation->setDataFi($dataFi);
+        $newReservation->setRechargeStation($station);
+        $newReservation->setUserReservation($user);
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->setContent($serializer->serialize($newReservation,'json'));
+        $response->setStatusCode(201);
+        return $response;
     }
 
     private function sameSpot(\App\Entity\Reservation $reservation, mixed $dataIni, mixed $dataFi)
     {
         $dataIniRes = $reservation->getDataIni();
         $dataFiRes = $reservation->getDataFi();
-        dd($dataIni,$dataIniRes,($dataIni <= $dataIniRes),($dataIniRes < $dataFi));
         if ((($dataIni <= $dataIniRes) && ($dataIniRes < $dataFi)) || (($dataIni <= $dataFiRes) && ($dataFiRes < $dataFi))) return true;
         return false;
+    }
+
+    #[Route('/reservations/update', name: 'update_reservation', methods: 'DELETE')]
+    public function actualitzaDB(ReservationRepository $reservationRepository, ManagerRegistry $doctrine)
+    {
+        $entityManager = $doctrine->getManager();
+        $reservations = $reservationRepository->findAll();
+
+        $at = date('Y-m-d H:i:s');
+        $format = 'Y-m-d H:i:s';
+        $actualTime = DateTimeImmutable::createFromFormat($format, $at);
+        foreach ($reservations as $reservation) {
+            if ($reservation->getDataFi() < $actualTime) {
+                $reservationRepository->remove($reservation);
+                $entityManager->flush();
+            }
+        }
     }
 }
