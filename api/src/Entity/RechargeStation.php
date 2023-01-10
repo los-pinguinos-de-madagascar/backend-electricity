322b009bef9c9080d9308769eaf6edaae7abc62e
Pol Blavia <pol.blavia@estudiantat.upc.edu>
2023-01-10 18:11:33 +0100

    Filtres done

--- api/src/Entity/RechargeStation.php
+++ api/src/Entity/RechargeStation.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\RechargeStationRepository;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -9,6 +10,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ApiResource]
+#[ApiFilter(\ApiPlatform\Doctrine\Orm\Filter\SearchFilter::class, properties: ["adress"=>"ipartial", "speedType"=>"ipartial", "connectionType"=>"ipartial", "power", "currentType"=>"ipartial"])]
 #[ORM\Entity(repositoryClass: RechargeStationRepository::class)]
 class RechargeStation extends Station
 {
